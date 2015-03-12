1da700b921936d77c7c37a18c90d4a08a51ca3c9
steveinatorx <steveinatorx@gmail.com>
2015-03-11 17:01:29 -0700

    change default postion to relative

--- spin.js
+++ spin.js
@@ -143,7 +143,7 @@
     className: 'spinner', // CSS class to assign to the element
     top: '50%',           // center vertically
     left: '50%',          // center horizontally
-    position: 'absolute'  // element position
+    position: 'relative'  // element position
   }
 
   /** The constructor */
